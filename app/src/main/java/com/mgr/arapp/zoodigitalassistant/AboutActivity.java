1d60396ab7451f934a310fd8e8564bc1c33cf00b
marpk92 <marpk92@gmail.com>
2018-07-14 01:00:26 +0200

    Dodanie ekranu informacji o aplikacji

---
+++ app/src/main/java/com/mgr/arapp/zoodigitalassistant/AboutActivity.java
@@ -0,0 +1,16 @@
+package com.mgr.arapp.zoodigitalassistant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Marcin on 14.07.2018.
+ */
+
+public class AboutActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.about_screen);
+    }
+}
